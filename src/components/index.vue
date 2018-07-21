1a9af4f552d8f6adf0520aea06404e79105559c1
sfmiller940 <nineforty@gmail.com>
2018-07-20 22:28:53 -0700

    Install Axios, connect to API

---
+++ src/components/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Brand name indices for less!</h2>
+    <div v-for="etf in etfs" :key="etf.id">{{ etf.symbol }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'MyFolio',
+      etfs: []
+    }
+  },
+  created () {
+    var self = this
+    axios.get('/api/etf').then((response) => {
+      console.log(response.data['etfs'])
+      self.etfs = response.data['etfs']
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
